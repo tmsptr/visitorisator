070d638d48128f41a887e3fe42aff657f770b959
Tomas Petruoka <tomas.petruoka@mif.stud.vu.lt>
2023-08-26 14:31:45 +0300

    failing backend tests fixed

--- backend/test.js
+++ backend/test.js
@@ -1,7 +1,7 @@
 const chai = require("chai");
 const chaiHttp = require("chai-http");
 const expect = chai.expect;
-const app = require("./app.js");
+const app = require("./index.js");
 
 chai.use(chaiHttp);
 
